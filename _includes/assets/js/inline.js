0c1db1ef94dc435650ae0f3dbb5f1d0980e0e6ac
Jeremy Karlsson <karlsson@jeremy.se>
2020-03-17 23:44:37 +0100

    Fix fallback

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,12 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  [...document.querySelectorAll('.plants-list img')]
+    .filter(img => img.naturalWidth === 0)
+    .forEach(img => {
+      img.setAttribute('src', '/static/img/fallback-plant.svg');
+      img.removeAttribute('srcset');
+    });
+});
