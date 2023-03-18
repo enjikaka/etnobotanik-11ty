2f67a33505688f2c02edf51f7ef33b07c5f3bb80
Jeremy Karlsson <karlsson@jeremy.se>
2023-03-18 18:25:40 +0100

    twekas

--- .eleventy.js
+++ .eleventy.js
@@ -24,6 +24,24 @@ async function imageShortcode(src, alt, sizes) {
   return Image.generateHTML(metadata, imageAttributes);
 }
 
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 module.exports = eleventyConfig => {
   eleventyConfig.addShortcode("image", imageShortcode);
 
@@ -35,6 +53,10 @@ module.exports = eleventyConfig => {
   eleventyConfig.addFilter("startsWith", (text, seq) => text?.startsWith(seq));
   eleventyConfig.addFilter("hasPlantPagesWithLetter", (plantPages, letter) => {
     return plantPages.filter(pp => pp.data.latinName?.startsWith(letter)).length > 0;
+  });
+  eleventyConfig.addFilter("randomCount", (items, limit) => {
+    console.log({ items, limit });
+    return shuffle(items).slice(0, limit);
   });
 
   eleventyConfig.addFilter("plantPagesLatinLetterSort", (plantPages, letter) => {
